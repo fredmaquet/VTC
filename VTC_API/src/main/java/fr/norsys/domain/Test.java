cc374b21b12bb9906d2917380af1537d44856bfd
MAQUET Frédéric <MAQUET Frdric>
2016-11-02 22:30:03 +0100

    ajout VTC-API

---
+++ VTC_API/src/main/java/fr/norsys/domain/Test.java
@@ -0,0 +1,18 @@
+package fr.norsys.domain;
+
+/**
+ * Created by fmaqu on 02/11/2016.
+ */
+
+public class Test {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
